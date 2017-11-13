f9f8b59d9a47b285901f3b81f754882639026f0a
Salanoid <salanoid@gmail.com>
2017-11-13 22:48:52 +0200

    add buttons and event to this buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
 public class Controller {
+    public void addCarButtonClicked () {
+        System.out.println("Add new Car!");
+    }
+
+    public void displayCarsButtonClicked() {
+        System.out.println("There are all the cars!");
+    }
+
+    public void deleteCarButtonClicked() {
+        System.out.println("Delete Car!");
+    }
+
+    public void closeProjectButtonClicked() {
+        System.out.println("Close Project!");
+    }
 }
