3e188efe943391fab650923f052ba9d494df1d42
Salanoid <salanoid@gmail.com>
2018-01-14 13:00:17 +0200

    some updates

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,15 +4,15 @@ import javafx.application.Platform;
 
 public class Controller {
     public void addCarButtonClicked () {
-        System.out.println("Add new Car!");
+        System.out.println("Buy car");
     }
 
     public void displayCarsButtonClicked() {
-        System.out.println("There are all the cars!");
+        System.out.println("There are all the cars");
     }
 
     public void deleteCarButtonClicked() {
-        System.out.println("Delete Car!");
+        System.out.println("Car Parts");
     }
 
     public void closeProjectButtonClicked() {
