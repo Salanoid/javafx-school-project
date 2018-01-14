bc40cddf4857187cc5fcea52c334b353aaa8a87e
Salanoid <salanoid@gmail.com>
2018-01-14 13:46:34 +0200

    contact form working

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,17 +2,26 @@ package sample;
 
 import javafx.application.Platform;
 
+import javax.swing.*;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 public class Controller {
+    String str;
     public void buyCarButtonClicked () {
         System.out.println("Buy car");
     }
 
     public void displayCarsButtonClicked() {
-        System.out.println("Display cars");
+
     }
 
-    public void contactButtonClicked() {
-        System.out.println("Contact");
+    public void contactButtonClicked() throws FileNotFoundException, UnsupportedEncodingException {
+        str = JOptionPane.showInputDialog("Leave us a message!");
+        PrintWriter writer = new PrintWriter("contact.txt", "UTF-8");
+        writer.println(str + '\n');
+        writer.close();
     }
 
     public void closeProjectButtonClicked() {
